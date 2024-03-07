418b77f9336618dc1d64a6805a5539866abdcb2a
Abdulloh <157456447+AbdullohSS@users.noreply.github.com>
2024-03-07 12:41:30 +0500

    Intro

---
+++ 1-dars/app.js
@@ -0,0 +1,131 @@
+//! Variable => o'zgaruvchi
+// let son = 13214;
+// console.log(son); // o'zgaruvchi
+// console.log("son"); // text
+
+//! Matematik amallar
+// console.log(son + 10);
+// console.log(son - 10);
+// console.log(son * 10);
+// console.log(son / 10);
+
+//! Ma'lumot turlari (Data-type)
+//? Primitive (Oddiy)
+//! Number
+// let son1 = 1234;
+// let son2 = 123.4;
+// console.log(typeof son1);
+// console.log(typeof son2);
+//! String
+// let string1 = "Hello world";
+// let string2 = 'Hello world';
+// let a = 32;
+// let string3 = `Hello world ${a}`;
+// let string1 = "Hello world", string2 = "Hello world", string3 = "Hello world";
+// console.log(string1, string2, string3);
+// console.log(string1);
+// console.log(string2);
+// console.log(string3);
+//! Boolean
+//? true, false
+// console.log(true);
+// console.log(false);
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(3 === 4);
+// console.log(typeof true);
+//! Null
+// let empty = null;
+// console.log(empty);
+//! Undefined
+// let empty;
+// console.log(empty);
+//! BigInt
+// let bigNumber = 9007199254740992n;
+// console.log(bigNumber * 2n);
+// console.log(typeof bigNumber);
+//! Symbol
+// let symbol1 = Symbol("Hello");
+// let symbol2 = Symbol("Hello");
+// console.log(symbol1 === symbol2);
+
+//? Non-primitive (oddiy bo'lmagan)
+// Object ( 1) Array, 2) Function, 3) Object)
+//! Array
+// let son = 1;
+// let string = "salom";
+// let array = [1, "salom", 2, "xayr", null, undefined];
+// console.log(typeof array);
+//! Function
+// function sayHello() {
+// 	console.log("Hello");
+// }
+// sayHello()
+//! Object
+// let object = {
+//   name: "John",
+//   age: 25,
+//   job: "Frontend developer",
+// };
+// console.log(object);
+
+// let son = 10;
+//! 1
+// son = 20;
+// console.log(son);
+//! 2
+// son = son + 10; // son = 10 + 10
+// son = son - 10; // son = 10 + 10
+// son = son * 10; // son = 10 + 10
+// son = son / 10; // son = 10 + 10
+// console.log(son);
+
+// let a = 12;
+// a = a * 2;
+// let b = 13;
+// b = b * 3;
+// console.log(a);
+// console.log(b);
+
+//! let, const, var
+// let a = 10;
+// a = 15;
+// console.log(a);
+
+// var a = 10;
+// a = 15;
+// console.log(a);
+
+// const a = 10;
+// a = 15;
+// console.log(a);
+// console.log(1);
+
+// var name = "John";
+// var name = "Sarah";
+// console.log(name);
+
+// let name = "John";
+// let name = "Sarah";
+// console.log(name);
+
+// const a = 10;
+// const a = 11;
+
+//! Primitive vs Non-primitive
+// let a = 10;
+// let b = "Hi";
+// let c = a;
+// c = c + 1;
+// console.log("a", a);
+// console.log("b", b);
+// console.log("c", c);
+
+// let a = 10;
+// let b = "Hi";
+// let c = {name: "Usmon"};
+// let d = c;
+// d.name = "Nosir";
+// console.log(c);
+// let firstName = "Ilya";
+// alert(`Hi ${firstName}`);
